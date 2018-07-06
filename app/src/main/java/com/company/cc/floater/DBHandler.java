28b2f01e96fe5630734b46ffb8fef2a06a6163ae
cririe <chayceririe@gmail.com>
2018-07-06 10:32:19 -0500

    clean up file structure, get rid of old DBHandler class

--- app/src/main/java/com/company/cc/floater/DBHandler.java
+++ app/src/main/java/com/company/cc/floater/DBHandler.java
@@ -1,29 +1,4 @@
 package com.company.cc.floater;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.content.Context;
-import android.content.ContentValues;
-import android.database.Cursor;
-
-public class DBHandler extends SQLiteOpenHelper {
-    //information of database
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "Floater.db";
-    public static final String TABLE_NAME = "";
-    public static final String COLUMN_ID = "";
-    public static final String COLUMN_NAME = "";
-    //initialize the database
-    public MyDBHandler(Context context, Stringname, SQLiteDatabase.CursorFactoryfactory, intversion) {
-        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
-    }
-    @Override
-    public void onCreate(SQLiteDatabase db) {}
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {}
-    public String loadHandler() {}
-    public void addHandler(Player player) {}
-    public Student findHandler(String playerName) {}
-    public boolean deleteHandler(int ID) {}
-    public boolean updateHandler(int ID, String name) {}
+public class DBHandler {
 }
