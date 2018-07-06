462ffb15a7e3b16865b5c8006ab47107d6265a2d
cririe <chayceririe@gmail.com>
2018-07-06 15:06:10 -0500

    connection to data

--- app/src/main/java/com/company/cc/floater/DBHandler.java
+++ app/src/main/java/com/company/cc/floater/DBHandler.java
@@ -1,4 +1,29 @@
 package com.company.cc.floater;
 
+import android.database.sqlite.SQLiteDatabase;
+
+
 public class DBHandler {
+    private SQLiteDatabase db;
+    private String dbLocation;
+
+    /**
+     * Creates and opens connection to database
+     */
+    public DBHandler(){
+        dbLocation = "android.resource://com.company.cc.floater/baseball_database";
+        db = SQLiteDatabase.openOrCreateDatabase(dbLocation,null,null);
+
+
+        return;
+    }
+
+    /**
+     * Closes database when it is no longer in use
+     *
+     */
+    public void CloseDatabase(){
+        db.close();
+        return;
+    }
 }
